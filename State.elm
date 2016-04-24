6556a0e471b9842a5ac5a9710dd2c53c80428ec7
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-04-24 12:04:35 -0700

    Move pieces down every time a scheduled shift occurs

--- State.elm
+++ State.elm
@@ -5,16 +5,24 @@ import Graphics.Collage exposing (..)
 import Graphics.Element exposing (Element)
 import Signal exposing (Signal)
 import Tetromino exposing (Tetromino)
+import Time exposing (Time)
 
 
 type alias State =
   { falling : Tetromino
+  , time : Time
+  , nextShift : Time
+  , shiftDelay : Time
   }
 
 
 defaultState : State
 defaultState =
-  { falling = Tetromino.j }
+  { falling = Tetromino.j
+  , time = 0
+  , nextShift = Time.second
+  , shiftDelay = Time.second
+  }
 
 
 view : State -> Element
@@ -32,6 +40,20 @@ view state =
     collage screenWidth screenHeight [ fallingForm ]
 
 
+{-| if it is time to do a shift, shift the piece down and update the
+scheduled nextShift time
+-}
+checkTick : State -> State
+checkTick state =
+  if (state.time < state.nextShift) then
+    state
+  else
+    { state
+      | falling = Tetromino.shift ( -1, 0 ) state.falling
+      , nextShift = state.time + state.shiftDelay
+    }
+
+
 update : Input -> State -> State
 update input state =
   case input of
@@ -44,6 +66,12 @@ update input state =
       { state
         | falling = Tetromino.shift amount state.falling
       }
+
+    Tick delta ->
+      checkTick
+        { state
+          | time = state.time + delta
+        }
 
 
 states : Signal State
