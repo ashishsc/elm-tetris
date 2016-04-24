80ec2e0198c1dace44a890775013b72e6535ea9d
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-04-24 15:43:51 -0700

    Add row clearing logic functions to board

--- Board.elm
+++ Board.elm
@@ -4,6 +4,7 @@ import Block exposing (Block)
 import Color
 import Dict exposing (Dict)
 import Graphics.Collage exposing (..)
+import Graphics.Element exposing (show)
 import Tetromino exposing (Tetromino, Location)
 
 
@@ -89,5 +90,91 @@ testBoard =
     ]
 
 
+cumulativeSum : List Int -> List Int
+cumulativeSum =
+  List.scanl (+) 0
+
+
+iota : Int -> List Int
+iota n =
+  List.repeat (n - 1) 1 |> cumulativeSum
+
+
+fillRow : Int -> Block -> Board -> Board
+fillRow row block board =
+  let
+    columns =
+      iota cols
+
+    rows =
+      List.repeat cols row
+
+    locations =
+      List.map2 (,) rows columns
+
+    blocks =
+      List.repeat cols block
+
+    filledRow =
+      List.map2 (,) locations blocks |> new
+  in
+    Dict.union filledRow board
+
+
+{-| Is the row complete?
+-}
+rowComplete : Int -> Board -> Bool
+rowComplete row board =
+  let
+    -- get the blocks for a specific row
+    blocks =
+      Dict.filter (\( r, _ ) _ -> r == row) board
+  in
+    -- check to see if the number of blocks in our row
+    -- matches the number of columns on the board
+    Dict.size blocks == cols
+
+
+{-| Remove a row from a board and shift all blocks above that row down
+-}
+clearRow : Int -> Board -> Board
+clearRow row board =
+  let
+    shift ( r, c ) block newBoard =
+      if (r < row) then
+        (Dict.insert ( r, c ) block newBoard)
+      else if (r > row) then
+        (Dict.insert ( r - 1, c ) block newBoard)
+      else
+        newBoard
+  in
+    Dict.foldr shift Dict.empty board
+
+
+clearLines : Board -> ( Int, Board )
+clearLines =
+  let
+    clearLines' row lines board =
+      if (row >= rows) then
+        -- base case
+        ( lines, board )
+      else if (rowComplete row board) then
+        clearLines' row (lines + 1) (clearRow row board)
+      else
+        clearLines' (row + 1) lines board
+  in
+    clearLines' 0 0
+
+
+test =
+  new []
+    |> fillRow 0 (Block Color.red)
+    |> fillRow 1 (Block Color.yellow)
+    |> fillRow 2 (Block Color.blue)
+    |> Dict.remove ( 1, 0 )
+    |> clearLines
+    |> snd
+
+
 main =
-  collage 600 600 [ toForm testBoard ]
+  collage 600 600 [ toForm test ]
