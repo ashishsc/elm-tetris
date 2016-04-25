79767f401b64e049ae814f4344ba84cd6ad1c2e0
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-04-24 21:50:09 -0700

    Add Board to State, add kicking logic

--- State.elm
+++ State.elm
@@ -1,5 +1,6 @@
 module State (..) where
 
+import Board exposing (Board)
 import Controller exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (Element)
@@ -10,6 +11,7 @@ import Time exposing (Time)
 
 type alias State =
   { falling : Tetromino
+  , board : Board
   , time : Time
   , nextShift : Time
   , shiftDelay : Time
@@ -18,11 +20,17 @@ type alias State =
 
 defaultState : State
 defaultState =
-  { falling = Tetromino.j
+  { falling = Tetromino.shift startingShift Tetromino.j
+  , board = Board.new []
   , time = 0
   , nextShift = Time.second
   , shiftDelay = Time.second
   }
+
+
+startingShift : ( Int, Int )
+startingShift =
+  ( 20, 5 )
 
 
 view : State -> Element
@@ -34,10 +42,10 @@ view state =
     screenHeight =
       600
 
-    fallingForm =
-      Tetromino.toForm state.falling
+    boardForm =
+      Board.addTetromino state.falling state.board |> Board.toForm
   in
-    collage screenWidth screenHeight [ fallingForm ]
+    collage screenWidth screenHeight [ boardForm ]
 
 
 {-| if it is time to do a shift, shift the piece down and update the
@@ -54,24 +62,101 @@ checkTick state =
     }
 
 
+useIfValid : State -> State -> State
+useIfValid current new =
+  if Board.isValid new.falling new.board then
+    new
+  else
+    current
+
+
+tryKicks : List ( Int, Int ) -> State -> State -> State
+tryKicks shifts current nextState =
+  case shifts of
+    [] ->
+      current
+
+    s :: rest ->
+      let
+        shifted =
+          Tetromino.shift s nextState.falling
+      in
+        if Board.isValid shifted nextState.board then
+          { nextState
+            | falling = shifted
+          }
+        else
+          tryKicks rest current nextState
+
+
+wallKick : State -> State -> State
+wallKick current nextState =
+  let
+    range =
+      nextState.falling.cols // 2
+
+    shifts =
+      [1..range] |> List.concatMap (\n -> [ ( 0, n ), ( 0, -n ) ])
+  in
+    tryKicks shifts current nextState
+
+
+floorKick : State -> State -> State
+floorKick current nextState =
+  let
+    range =
+      nextState.falling.rows // 2
+
+    shifts =
+      [1..range] |> List.map (\n -> ( n, 0 ))
+  in
+    tryKicks shifts current nextState
+
+
 update : Input -> State -> State
 update input state =
-  case input of
-    Rotate ->
-      { state
-        | falling = Tetromino.rotate state.falling
-      }
+  let
+    useIfValid' =
+      useIfValid state
+  in
+    case input of
+      Rotate ->
+        let
+          rotated =
+            { state
+              | falling = Tetromino.rotate state.falling
+            }
 
-    Shift amount ->
-      { state
-        | falling = Tetromino.shift amount state.falling
-      }
+          nextState =
+            useIfValid' rotated
 
-    Tick delta ->
-      checkTick
-        { state
-          | time = state.time + delta
-        }
+          -- Try various kicks if the state didn't change
+          nextState' =
+            if nextState == state then
+              wallKick state rotated
+            else
+              nextState
+
+          nextState'' =
+            if nextState' == state then
+              floorKick state rotated
+            else
+              nextState'
+        in
+          nextState''
+
+      Shift amount ->
+        useIfValid'
+          { state
+            | falling = Tetromino.shift amount state.falling
+          }
+
+      Tick delta ->
+        useIfValid'
+          <| checkTick
+              { state
+                | time = state.time + delta
+              }
 
 
 states : Signal State
